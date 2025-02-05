148cef51034cf1d792f07a4c7ef9bd1828ee46ff
VamshiNallavalli <vamshikrishnaiam7@gmail.com>
2025-02-05 12:24:34 +0530

    Streams

---
+++ JAVA_SE_8/Lambda Expressions/Using_Simple_Lambda/src/Practice.java
@@ -0,0 +1,12 @@
+public class Practice {
+    public static void main(String[] args) {
+        in p=()->{
+            System.out.println("Hii");
+        };
+        p.hi();
+    }
+}
+
+interface in{
+    public void hi();
+}
